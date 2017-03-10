27f6f1d70abd5a71bb6014540fe5be486e3b9087
Tamo Tkhlashidze <tkhlashidzetamo0@gmail.com>
2017-03-10 04:22:12 +0400

    scales

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var parseDate = d3.time.format("%m/%d/%Y").parse;
 var parsedData = [];
-var groupedData = {};
 
+var yearlyData = [];
 
 var config  = {
       svgWidth : 800,
@@ -15,16 +15,29 @@ var config  = {
 };
 
 var lineColors = {
-    "2012" : "red",
-    "2013" : "green",
-    "2014" : "blue",
-    "2015" : "black",
-    "2016" : "pink",
-    "2017" : "grey",
+    "0" : "red",
+    "1" : "green",
+    "2" : "blue",
+    "3" : "black",
+    "4" : "pink",
+    "5" : "grey",
+    "6" : "teal",
+     "7" : "orange",
 };
 
-var monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
+var monthNames = [
+    {n: 1, name :"January"},
+    {n: 2, name :"February"},
+    {n: 3, name :"March"},
+    {n: 4, name :"April"},
+    {n: 5, name :"May"},
+    {n: 6, name :"June"},
+    {n: 7, name :"July"},
+    {n: 8, name :"August"}, 
+    {n: 9, name :"September"},
+    {n: 10, name :"October"},
+    {n: 11, name :"November"},
+    {n: 12, name :"December"}
 ];
 
 var width = config.svgWidth - config.svgMargin.left - config.svgMargin.right;
@@ -50,78 +63,62 @@ d3.csv("currency_data.csv")
     .get(function(error, callBackData) { 
         if(error) throw error;
         parsedData = callBackData;
-        groupccyData();
+        ProcessData();
      });
 
 
-function groupccyData(){
-    // console.log("parsedData  " );
-    // console.log(parsedData);
-    
+function ProcessData(){
     for(item of parsedData){
-       if( groupedData[item.date.getFullYear()] == undefined){
-             groupedData[item.date.getFullYear()] = [];
-       }
-       groupedData[item.date.getFullYear()].push(item);
-    }
+        var fullYear = item.date.getFullYear() + "";
+        var year = fullYear.substring(3, 4);
+        
+        if(yearlyData[year] == undefined) {
+           yearlyData[year] = [];
+        }
+       yearlyData[year].push(item);
+   }
 
-    AddLines();
-    // console.log("groupedData");
-    // console.log(groupedData);
+    PaintChart();
+}
 
- }
-
-function AddLines(){
+function PaintChart(){
 
 
-for(yearData in groupedData){
-    
 var xScale = d3.time.scale()
-                .domain(d3.extent(groupedData[yearData], function(d) { return d.date; }))
+                .domain(d3.extent(monthNames, function(d) { return d.n; }))
                 .range([0,width]);
 
 var yScale = d3.scale.linear()
-               .domain(d3.extent(groupedData[yearData], function(d) { return d.value; }))
-               .range([ height,0]);
-
-
-var line = d3.svg.line()
-    .x(function(d) { return xScale(d.date); })
-    .y(function(d) { return yScale(d.value); });
-
-g.append("path")
-      .datum(groupedData[yearData])
-      .attr("fill", "none")
-      .attr("stroke", lineColors[yearData])
-      .attr("stroke-linejoin", "square")
-      .attr("stroke-linecap", "square")
-      .attr("stroke-width", 1.5)
-      .attr("d", line);
-
-}
-
-
-
-    
-var globalXScale = d3.time.scale()
-                .domain([new Date(2012, 0, 1), new Date(2012, 11, 31)])
-                .range([0,width]);
-
-var globalYScale = d3.scale.linear()
                .domain(d3.extent(parsedData, function(d) { return d.value; }))
                .range([ height,0]);
 
 
+var lines  = g.selectAll("path")
+                   .data(yearlyData)
+                   .enter()
+                   .append("path")
+                        .datum(function(d,i) { return d; })
+                        .attr("fill", "none")
+                        .attr("stroke", function(d,i) { return lineColors[i]; })
+                        .attr("stroke-linejoin", "square")
+                        .attr("stroke-linecap", "square")
+                        .attr("stroke-width", 1.5)
+                        .attr("d", d3.svg.line()
+                                    .x(function(d) { return xScale(d.date.getMonth()+1); })
+                                    .y(function(d) { return yScale(d.value); })
+                             );
+
+
+
 var xAxis = d3.svg.axis()
-              .scale(globalXScale)
+              .scale(xScale)
               .orient("bottom")
               .ticks(d3.time.months)
               .tickFormat(d3.time.format("%B"));
 
 var yAxis = d3.svg.axis()
-              .scale(globalYScale)
+              .scale(yScale)
               .orient("left");
-
 
               
    g.append("g")
@@ -154,93 +151,3 @@ var yAxis = d3.svg.axis()
 
 
 }
-
-
-
-
-// var xScale = d3.time.scale()
-//                 .range([0,width]);
-
-// var yScale = d3.scale.linear()
-//                .range([ height,0]);
-
-
-// var xAxis = d3.svg.axis()
-//               .scale(xScale)
-//               .orient("bottom");
-
-// var yAxis = d3.svg.axis()
-//               .scale(yScale)
-//               .orient("left");
-
-
-// // var line = d3.svg.line()
-// //     .x(function(d) { return xScale(d.date); })
-// //     .y(function(d) { return yScale(d.close); });
-
-
-// var area = d3.svg.area()
-//     .x(function(d) { return xScale(d.date); })
-//     .y1(function(d) { return yScale(d.close); });
-
-
-// d3.tsv("data.tsv", function(error,callbackData) {
-//   if (error) throw error;
-
-//   var parseDate = d3.time.format("%d-%b-%y").parse;
- 
-//   callbackData.forEach(function(d) {
-//     d.date = parseDate(d.date);
-//     d.close = +d.close;
-//     });
-
-//    console.log(callbackData);
-
-//    xScale.domain(d3.extent(callbackData, function(d) { return d.date; }));
-//    yScale.domain(d3.extent(callbackData, function(d) { return d.close; })); 
-
-//    area.y0(height);
-
-//    g.append("path")
-//         .datum(callbackData)
-//         .attr("fill", "steelblue")
-//         .attr("d", area);
-
-
-//    g.append("g")
-//             .attr("class", "axis")
-//             .attr("transform", "translate(0," + height  + ")")
-//             .call(xAxis)
-//     .append("text")
-//             .attr("x", width-config.svgMargin.right)
-//             .attr("y", -5)
-//             .style("text-anchor", "end")
-//             .text("Date");
-
-
-
-//     g.append("g")
-//             .attr("class", "axis")
-//             .call(yAxis)
-//      .append("text")
-//             .attr("transform", "rotate(-90)")
-//             .attr("y", 10)
-//             .attr("dy", ".71em")
-//             .style("text-anchor", "end")
-//             .text("Price ($)");
-
-//     // g.append("path")
-//     //   .datum(callbackData)
-//     //   .attr("fill", "none")
-//     //   .attr("stroke", "teal")
-//     //   .attr("stroke-linejoin", "square")
-//     //   .attr("stroke-linecap", "square")
-//     //   .attr("stroke-width", 1.5)
-//     //   .attr("d", line);
-// });
-
-
-
- 
-
-
